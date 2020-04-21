fb0122c29bec7cda296e469d5155895940f62b8d
plaird730 <plaird730@gmail.com>
2020-04-20 20:45:33 -0400

    Forcing a push again...

--- routes/ping.js
+++ routes/ping.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-// Test route to make sure api is working.
+// Test route for API.
 router.get("/", function (req, res, next) {
   res.json({ message: "Hello from the Above Curve API!" });
 });
