1fa0d1bf742bac67d0076fe0ad26d5003744ba35
plaird730 <plaird730@gmail.com>
2020-04-20 20:34:11 -0400

    Added base path instructions via json

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+// api.abovecurve.com/
 router.use("/health", require("./health"));
-router.use("/", require("./ping"));
+router.use("/ping", require("./ping"));
+
+router.get("/", function (req, res, next) {
+  res.json({
+    welcome:
+      "Welcome to the Above Curve API. Here you will find data-sets on various fields of research.",
+    dataSets: ["health"],
+    links: [{ health: `/health` }],
+  });
+});
 
 module.exports = router;
