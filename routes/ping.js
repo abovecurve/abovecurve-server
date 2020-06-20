aedf0ac8ac802d540416ed05cca2dd31ec8e6b15
paullnh <plaird83@gmail.com>
2020-06-20 08:37:15 -0400

    logging env variables for testing

--- routes/ping.js
+++ routes/ping.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 // Test route for API.
 router.get("/", function (req, res, next) {
-  res.json({ message: "Hello from the Above Curve API!" });
+  res.json({
+    message: "Hello from the Above Curve API!",
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME
+  });
 });
 
 module.exports = router;
